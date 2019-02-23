8c13ffcecfd880c07ef84ad0691add30ea3bbe47
Gareth Halfacree <gareth@halfacree.co.uk>
2019-02-23 15:30:59 +0000

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # unicorn-weather
 Weather icon system for the Pimoroni Unicorn HAT
+
+weather-icons.py and icons/* (C) 2016 LoveBootCaptain, MIT Licence
+get-weather.py (C) 2019 Gareth Halfacree, BSD 3-Clause Licence
+
+Downloads weather data from OpenWeatherMap, displays it on a connected Pimoroni UnicornHAT.
+
+Notes:
+Only runs from 0700 to 2100 (so you're not blinded at night.)
+Loops inifinitely.
+Fairly gross handling of OWM's various condition codes.
+Requires 'requests' Python library.
---
+++ get-weather.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import requests,subprocess,time
+
+params = (
+    ('q', 'ENTER-LOCATION-HERE'),
+    ('appid', 'ENTER-API-KEY-HERE'),
+)
+
+while True:
+    currenthour=int(time.strftime("%H"))
+    if (currenthour >= 7) and (currenthour < 21):
+        try:
+            response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
+        except requests.exceptions.RequestException as e:
+            print(e)
+        weatherCode = response.json().get('weather')[0].get('id')
+
+        if weatherCode < 600:
+            weatherIcon = 'rain.png'
+        elif weatherCode < 700:
+            weatherIcon = 'snow.png'
+        elif weatherCode < 771:
+            weatherIcon = 'fog.png'
+        elif weatherCode < 782:
+            weatherIcon = 'wind.png'
+        elif weatherCode == 800:
+            weatherIcon = 'clear-day.png'
+        elif weatherCode < 804:
+            weatherIcon = 'partly-cloudy-day.png'
+        elif weatherCode == 804:
+            weatherIcon = 'cloudy.png'
+
+        subprocess.call(['python', '/home/pi/weather/weather-icons.py', weatherIcon])
+
+    else:
+        time.sleep(60)
